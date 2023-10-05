5c54c4b1b0b55ba0018e20724858f328434f8c00
sval4 <val.sharat@gmail.com>
2023-10-05 00:30:06 -0400

    Store pdf from url, add pdf info to vector db

--- ingest.py
+++ ingest.py
@@ -4,13 +4,20 @@ from langchain.embeddings import HuggingFaceEmbeddings
 from langchain.vectorstores import FAISS
 from langchain.document_loaders import UnstructuredURLLoader
 
+import torch
 import requests
 import mechanicalsoup
 from bs4 import BeautifulSoup
 import time
 import xml.etree.ElementTree as ET
+import os
+
+DEVICE = "cpu"
+if torch.cuda.is_available():
+    DEVICE = "cuda"
 
 DB_FAISS_PATH = "vectorstores/db_faiss"
+DATA_PATH = "data/"
 
 def listOfCenters(browser, headers):
     delay = 2
@@ -24,22 +31,24 @@ def listOfCenters(browser, headers):
     links = page.soup.find_all('a')
     for link in links:
         href = link.get('href')
-        if href and "http" in href and href.lower().find("nysdra") == -1 \
+        if href and ".pdf" in href:
+            if not os.path.exists(DATA_PATH):
+                os.makedirs(DATA_PATH)
+
+            response = requests.get(href)
+            filename = href[href.rfind("/") + 1:]
+            filepath = os.path.join(DATA_PATH, filename)
+            with open(filepath, "wb") as f:
+                f.write(response.content)
+
+        elif href and "http" in href and href.lower().find("nysdra") == -1 \
         and href.lower().find("youtube") == -1 and href.lower().find("linkedin") == -1 and href.lower().find("map") == -1:
             listOfCenters.add(href)
+
     print(len(listOfCenters))
     return list(listOfCenters)
 
 def createVectorDB():
-    # loader = DirectoryLoader(DATA_PATH, glob="*.pdf", loader_cls=PyPDFLoader)
-    # documents = loader.load()
-    # URLs=[
-    # 'https://www.britannica.com/animal/penguin',
-    # 'https://www.livescience.com/animals/birds/penguins',
-    # 'https://www.nationalgeographic.com/animals/birds/facts/penguins-1',
-    # 'https://ocean.si.edu/ocean-life/seabirds/penguins'
-
-    # ]
     headers = {
         'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3'
     }
@@ -50,13 +59,22 @@ def createVectorDB():
 
     loaders=UnstructuredURLLoader(urls=URLs, headers=headers)
     documents=loaders.load()
+    #doc = Document(page_content="text", metadata={"source": "url"})
 
     text_splitter = RecursiveCharacterTextSplitter(chunk_size = 500, chunk_overlap = 50)
     texts = text_splitter.split_documents(documents)
-    #I think you have to do {"device": "cuda"} in order to use GPU
-    embeddings = HuggingFaceEmbeddings(model_name = 'sentence-transformers/all-MiniLM-L6-v2', model_kwargs={"device": "cpu"})
+    embeddings = HuggingFaceEmbeddings(model_name = 'sentence-transformers/all-MiniLM-L6-v2', model_kwargs={"device": DEVICE})
 
     db = FAISS.from_documents(texts, embeddings)
+
+    loader = DirectoryLoader(DATA_PATH, glob="*.pdf", loader_cls=PyPDFLoader)
+    documents = loader.load()
+    texts = text_splitter.split_documents(documents)
+
+    dbpdf = FAISS.from_documents(texts, embeddings)
+
+    db.merge_from(dbpdf)
+
     db.save_local(DB_FAISS_PATH)
 
 if __name__ == "__main__":
