53e5b29472a3e4f69255e38572041ed6cbc71ad3
kismet37 <ofkismetandkalon@gmail.com>
2023-12-05 15:59:35 -0500

    Fixed data_cleaner bugs
    Also processed the web files

--- data_cleaner.py
+++ data_cleaner.py
@@ -6,11 +6,12 @@ import string
 
 def process_words():
     d = enchant.Dict("en_US")
-    directory = 'test_files'
+    directory = 'web_files'
 
     #need to go through the files in the directory
+    file_index = 1
     for filename in os.listdir(directory):
-        file_index = 1
+        
         f = os.path.join(directory, filename)
 
         #hypothetically now we have a file to process
@@ -24,44 +25,57 @@ def process_words():
             i = 0
             while i < len(words):
                 print(words[i])
+                #stripping punctuation so the word check will work, but saving it to make sure that we don't just get rid of all punctuation
                 no_punc = words[i].translate(str.maketrans('','',string.punctuation))
-                if (d.check(no_punc) == False):
+                if(len(no_punc) > 0):
+                    if (d.check(no_punc) == False):
 
-                    if(i < (len(words)-1)):
-                        no_punc_next = words[i+1].translate(str.maketrans('','',string.punctuation))
-                        print("TRYING" + " " + no_punc+no_punc_next)
-                        if (d.check((no_punc+no_punc_next))):
-                            print("YUH")
-                            clean_text += words[i] + words[i+1] + " "
-                            i+=1
+                        #first try combining it with the next word chunk
+                        if(i < (len(words)-1)):
+                            no_punc_next = words[i+1].translate(str.maketrans('','',string.punctuation))
+                            if(len(no_punc_next) > 0):
+                                print("TRYING" + " " + no_punc+no_punc_next)
+                                if (d.check((no_punc+no_punc_next))):
+                                    print("YUH")
+                                    clean_text += words[i] + words[i+1] + " "
+                                    i+=1
 
-                        else:
-                            #otherwise try it with the previous words 
-                            no_punc_prev = words[i-1].translate(str.maketrans('','',string.punctuation))
-                            print("TRYING" + " " + no_punc_prev+no_punc)
-                            if (d.check((no_punc_prev+no_punc))):
-                                print("YUH")
-                                del_ind = len(clean_text)-len(words[i-1])
-                                clean_text = clean_text[0:del_ind-1]
-                                clean_text += words[i-1] + words[i] + " "
+                            else:
+                                #otherwise try it with the previous word bit (which may have been counted as a vlaid word)
+                                if(i > 0):
+                                    no_punc_prev = words[i-1].translate(str.maketrans('','',string.punctuation))
+                                    if(len(no_punc_prev) > 0):
+                                        print("TRYING" + " " + no_punc_prev+no_punc)
+                                        if (d.check((no_punc_prev+no_punc))):
+                                            print("YUH")
+                                            del_ind = len(clean_text)-len(words[i-1])
+                                            clean_text = clean_text[0:del_ind-1]
+                                            clean_text += words[i-1] + words[i] + " "
 
-                else:
-                    clean_text += words[i] + " "
+
+
+                                else:
+                                    #just add it anyway because sometimes courts make up words
+                                    clean_text += words[i] + " "
+
+
+
+                    else:
+                        clean_text += words[i] + " "
+
                 
                 i+=1
-            # print("WORDS")
-            # for word in text:
-            #     print(word)
+ 
 
 
             #write cleaned data to new file
             filename = "file" + str(file_index) + ".txt"
-            to_file = open("cleaned_txt_files/"+filename, "w")
+            to_file = open("cleaned_web_files/"+filename, "w")
 
             to_file.write(clean_text)
             to_file.close()
     
-    file_index+=1
+        file_index+=1
 
 def main():
     process_words()
